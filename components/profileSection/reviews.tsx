6f1c198c3641a60da19333fd74a35251ae4415b1
Toniodujinrin <todujinrin@gmail.com>
2023-05-19 14:09:46 -0600

    updates

--- components/profileSection/reviews.tsx
+++ components/profileSection/reviews.tsx
@@ -1,9 +1,31 @@
-const Reviews = () => {
+import { useContext } from "react";
+import { ProfileContext } from "../../Contexts/ProfileContext";
+import Reviews from "../accountSection/reviews";
+const ReviewsComp = () => {
+  const {reviews} = useContext(ProfileContext)
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex flex-col">
+      {
+        <Reviews/>
+
+      }
+      
+      <div className="w-full flex justify-end">
+      <button
+          onClick={() =>{}}
+          className={`w-[170px] p-2 mt-[20px] items-center border-2 
+           
+         bg-forestGreen border-forestGreen 
+          
+           text-white cursor-pointer  rounded-[20px]`}
+        >
+          <p> Write Review</p>
+        </button>
+      </div>
+      
     </div>
   );
 };
 
-export default Reviews;
+export default ReviewsComp;
