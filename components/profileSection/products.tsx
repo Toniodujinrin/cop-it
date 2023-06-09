7afbb99f3013dbde5ba9ddb54f79e7f3c090b74e
Toniodujinrin <todujinrin@gmail.com>
2023-06-08 22:06:05 -0600

    deploy error fix

--- components/profileSection/products.tsx
+++ components/profileSection/products.tsx
@@ -1,13 +1,16 @@
-import { Provider, useContext } from "react";
+import { Provider, useContext, useState } from "react";
 import { ProfileContext } from "../../Contexts/ProfileContext";
 import { Product } from "../../types";
 import ProductBox from "../productBox";
 const Products = () => {
   const { products } = useContext(ProfileContext);
+  const [quickViewProduct, setQuickViewProduct]= useState('')
   return (
     <div className="grid lg:grid-cols-4 gap-x-4 gap-y-4 justify-items-center grid-cols-2 ">
       {products.map((item: Product) => (
         <ProductBox
+        setQuickViewProduct={setQuickViewProduct}
+          showQuickViewIcon={false}
           productId={item._id}
           name={item.name}
           price={item.price}
